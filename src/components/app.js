ebb8485bd21e6095613606c0c9fee45841752639
Cody Brunner <rockchalkwushock@icloud.com>
2016-12-09 08:32:27 -0600

    Recloned repo and starting over.

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
-import Header from './header';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Header />
-        {this.props.children}
-      </div>
+      <div>React simple starter</div>
     );
   }
 }
